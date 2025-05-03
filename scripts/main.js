403b21753018673fb73b493edda9eafffbc308ac
Jeido <tourais.q@gmail.com>
2025-05-03 14:06:17 +0200

    initialise ws server per client + init payload

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,33 @@
-import { WebSocketServer } from 'ws';
-
-let wss;
-
 Hooks.once("ready", () => {
-  if (!game.user.isGM) return; // Un seul utilisateur démarre le serveur
+  const socket = new WebSocket("ws://localhost:35601");
 
-  wss = new WebSocketServer({ port: 35600 }); // Port local
+  socket.onopen = () => {
+    console.log("[RP] Connecté au client RichPresence");
 
-  wss.on("connection", (ws) => {
-    console.log("RichPresence connecté");
+    setInterval(() => {
+      const actor = game.actors.get(game.user.character?.id);
+      const classId = actor?.system?.details?.originalClass;
+      const classItem = actor?.items?.find(item => item.type === "class" && item._id === classId);
+      const payload = {
+        userName: game.user.name,
+        scene: game.scenes.active?.name ?? "???",
+        inCombat: game.combat?.started ?? false,
+        actorName: actor?.name ?? null,
+        hp: actor?.system?.attributes?.hp,
+        isGM: game.user.isGM,
+        totalUsers: game.users.size,
+        onlineUsers: game.users.filter(e => e.active).length,
+        worldName: game.data.world.title,
+        className: classItem?.name,
+        classLevel: classItem?.system.levels,
+        worldId: game.world?.id,
+        sceneId: game.scenes.active?.id
+      };
+      socket.send(JSON.stringify(payload));
+    }, 2000);
+  };
 
-    const sendData = () => {
-      const data = getCurrentRichPresenceData();
-      ws.send(JSON.stringify(data));
-    };
-
-    // Envoyer les données à l'ouverture
-    sendData();
-
-    // Re-envoyer périodiquement ou via events
-    const interval = setInterval(sendData, 2000);
-
-    ws.on("close", () => clearInterval(interval));
-  });
+  socket.onerror = (err) => {
+    console.warn("[RP] Échec de connexion WebSocket :", err);
+  };
 });
