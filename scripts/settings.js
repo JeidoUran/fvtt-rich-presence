77b9622c59570ad85ef74d8a0c275f5ef995eef6
Jeido <tourais.q@gmail.com>
2025-05-04 17:06:26 +0200

    add button leading to client download page

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,4 +1,12 @@
 Hooks.once("init", () => {
+    game.settings.registerMenu("fvtt-rich-presence", "clientDownload", {
+        name: "RICHPRESENCE.Settings.ClientDownload.Name",
+        hint: "RICHPRESENCE.Settings.ClientDownload.Hint",
+        label: "RICHPRESENCE.Settings.ClientDownload.Label",
+        icon: "fa-solid fa-download",
+        type: ClientDownload,
+        restricted: false
+    });
     game.settings.register("fvtt-rich-presence", "enableRichPresence", {
         name: "RICHPRESENCE.Settings.Enable.Name",
         hint: "RICHPRESENCE.Settings.Enable.Hint",
@@ -10,3 +18,17 @@ Hooks.once("init", () => {
       });
   });
   
+  class ClientDownload extends FormApplication {
+    static get defaultOptions() {
+        return mergeObject(super.defaultOptions, {
+            id: "fvtt-rich-presence-client-download",
+            title: "ClientDownload",
+            template: "templates/settings/menu.html"
+        });
+    }
+
+    render() {
+        window.open("https://github.com/JeidoUran/fvtt-player-client/releases", "_blank");
+        return null;
+    }
+}
