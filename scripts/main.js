829fa6bb0738f4d3f0f0ce485246f1459ae3091f
Jeido <tourais.q@gmail.com>
2025-05-05 21:53:42 +0200

    add dnd5e multiclassing support and pf2e support

--- scripts/main.js
+++ scripts/main.js
@@ -31,22 +31,42 @@ Hooks.once("ready", () => {
     console.log("[RP] Connected to RichPresence Client");
     ui.notifications.notify(game.i18n.localize("RICHPRESENCE.Notifications.Connected"));
     window._richPresenceInterval = setInterval(() => {
-        const actor = game.actors.get(game.user.character?.id);
-        const classId = actor?.system?.details?.originalClass;
-        const classItem = actor?.items?.find(item => item.type === "class" && item._id === classId);
+        const sys       = game.system.id;
+        const actor     = game.actors.get(game.user.character?.id);
+        const pf2eClass = actor?.class;
+        
+        const classItems = sys === "dnd5e"
+          ? actor?.items.filter(i => i.type === "class") || []
+          : sys === "pf2e"
+            ? [pf2eClass].filter(Boolean)
+            : [];
+        
+        const classDisplay = classItems.length > 0
+          ? classItems
+          .map(ci => {
+            let lvl = null;
+            if (sys === "dnd5e") {
+              lvl = ci.system?.levels;
+            } else if (sys === "pf2e") {
+              lvl = actor.system?.details?.level?.value;
+            }
+            return `${ci.name}${lvl != null ? ` Level ${lvl}` : ""}`;
+          })
+          .join(" / ")
+      : null;
+
         const payload = {
-          userName: game.user.name,
-          inCombat: game.combat?.started ?? false,
-          actorName: actor?.name ?? null,
-          hp: actor?.system?.attributes?.hp,
-          isGM: game.user.isGM,
-          totalUsers: game.users.size,
-          onlineUsers: game.users.filter(e => e.active).length,
-          worldName: game.data.world.title,
-          className: classItem?.name,
-          classLevel: classItem?.system.levels,
-          worldId: game.world?.id,
-          sceneId: game.scenes.active?.id
+          userName:      game.user.name,
+          inCombat:      game.combat?.started ?? false,
+          actorName:     actor?.name ?? null,
+          hp:            actor?.system?.attributes?.hp,
+          isGM:          game.user.isGM,
+          totalUsers:    game.users.size,
+          onlineUsers:   game.users.filter(u => u.active).length,
+          worldName:     game.data.world.title,
+          className:     classDisplay,
+          worldId:       game.world?.id,
+          sceneId:       game.scenes.active?.id
         };
         ws.send(JSON.stringify(payload));
       }, 5000);
